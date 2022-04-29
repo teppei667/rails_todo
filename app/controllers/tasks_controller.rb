37ae4112635b8f105d06061bfccb6b30fe650352
teppei667 <teptympei.sasstjgada.666@gmail.com>
2022-04-29 10:32:55 +0900

    Add delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,7 +26,13 @@ class TasksController < ApplicationController
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
   end
-  
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました."
+  end
+
   private
 
   def task_params
