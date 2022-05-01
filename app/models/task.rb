5dab2a1bdcf654cb81ae67cc6c907490fb97a7c5
teppei667 <teptympei.sasstjgada.666@gmail.com>
2022-05-01 16:31:13 +0900

    Add user model & migration

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  before_validation :set_nameless_name
+
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
@@ -6,5 +8,9 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることができません') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = 'non title' if name.blank?
   end
 end
