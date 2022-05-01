27ad1d36c7764763e1edf85606cc0be9bdf1c05f
teppei667 <teptympei.sasstjgada.666@gmail.com>
2022-05-01 23:09:28 +0900

    Add users controller CRUD

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,11 @@
 class Admin::UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -19,10 +21,23 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-  def update
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}を更新しました。」"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}を削除しました。」"
   end
 
 
