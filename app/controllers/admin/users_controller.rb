9195f5bfcfb20315b7a0198bd6499ee18b354854
teppei667 <teptympei.sasstjgada.666@gmail.com>
2022-05-01 23:03:21 +0900

    Add make user

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,37 @@
+class Admin::UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+end
