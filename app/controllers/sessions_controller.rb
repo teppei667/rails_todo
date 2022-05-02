4215a8fb8888113ff8d2ae627462984a96875d87
teppei667 <teptympei.sasstjgada.666@gmail.com>
2022-05-02 10:45:01 +0900

    Add login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました'
     else
       render :new
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました'
   end
 
   private
