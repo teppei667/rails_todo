8568c7d079f16c89cdcddaff13b49b3520c9d587
teppei667 <teppei.engineer.667@gmail.com>
2022-06-03 23:24:06 +0900

    Add task create test

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -39,4 +39,29 @@ describe 'タスク管理機能', type: :system do
       it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
+
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit new_task_path
+      fill_in 'Name', with: task_name
+      click_button '登録する'
+    end
+    
+    context '新規作成画面で名称を入力したとき' do
+      let(:task_name) { '新規作成のテストを書く' }
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかったとき' do
+      let(:task_name) { '' }
+      it 'non titleで登録される' do
+        visit tasks_path
+        expect(page).to have_content 'non title'
+      end
+    end
+  end
 end
